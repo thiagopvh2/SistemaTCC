80408c0009483cc10955d70aff5468a0e10e9012
Thiago Salvador Soares <thiagopvh2@gmail.com>
2018-08-25 19:19:28 -0400

    CRUD lançamento

---
+++ ProjetoTCC/Controllers/LancamentosController.cs
@@ -0,0 +1,128 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoTCC.Context;
+using ProjetoTCC.Models;
+
+namespace ProjetoTCC.Controllers
+{
+    public class LancamentosController : Controller
+    {
+        private ProjetoTccContext db = new ProjetoTccContext();
+
+        // GET: Lancamentos
+        public ActionResult Index()
+        {
+            return View(db.Lancamentos.ToList());
+        }
+
+        // GET: Lancamentos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lancamento lancamento = db.Lancamentos.Find(id);
+            if (lancamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lancamento);
+        }
+
+        // GET: Lancamentos/Criar
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+        // POST: Lancamentos/Criar
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Criar([Bind(Include = "Id,Descricao,Data,Tipo,Valor")] Lancamento lancamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lancamentos.Add(lancamento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(lancamento);
+        }
+
+        // GET: Lancamentos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lancamento lancamento = db.Lancamentos.Find(id);
+            if (lancamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lancamento);
+        }
+
+        // POST: Lancamentos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descricao,Data,Tipo,Valor")] Lancamento lancamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lancamento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lancamento);
+        }
+
+        // GET: Lancamentos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lancamento lancamento = db.Lancamentos.Find(id);
+            if (lancamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lancamento);
+        }
+
+        // POST: Lancamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lancamento lancamento = db.Lancamentos.Find(id);
+            db.Lancamentos.Remove(lancamento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ProjetoTCC/Models/Lancamento.cs
+++ ProjetoTCC/Models/Lancamento.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,17 @@ namespace ProjetoTCC.Models
 {
     public class Lancamento
     {
+        [Key]
         public int Id { get; set; }
+        [DisplayName("Descrição")]
         public string Descricao { get; set; }
+        [DisplayName("Data de registro")]
+        [Required(ErrorMessage = "A data não pode ficar em branco!")]
         public DateTime Data { get; set; }
+        [DisplayName("Tipo (Entrada ou Saída?)")]
         public bool Tipo { get; set; }
+        [DisplayName("Valor")]
+        [Required(ErrorMessage = "O valor' não pode ficar em branco!")]
         public double Valor { get; set; }
     }
 }
